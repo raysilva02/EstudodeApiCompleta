2bd311670efa4f7ed93b31cdcdf3bf548c205759
Rayana Silva <rayanacdasilva@gmail.com>
2024-06-11 23:19:05 -0300

    Adicionar arquivos de projeto.

---
+++ Model/ApiDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EstudoApiCompleta.Model
+{
+    public class ApiDbContext : DbContext
+    {
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
---
+++ Model/Cliente.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EstudoApiCompleta.Model
+{
+    public class Cliente
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage ="O campo {0} é obrigatório")]
+        [StringLength (100, ErrorMessage = "O campo {0} deve possuir entre {1} e {2} caracteres",  MinimumLength = 6)]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public int TipoCliente { get; set; }
+
+        public bool Ativo { get; set; }
+
+    }
+}
